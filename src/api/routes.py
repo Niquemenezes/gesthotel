7baddc9b25d88735405c4b537cae8021c675d866
Monique Menezes <122538719+Niquemenezes@users.noreply.github.com>
2025-03-06 09:12:11 +0000

    back hotel

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Hoteles
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -20,3 +20,54 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/hoteles', methods=['GET'])
+def obtener_hoteles():
+    hoteles = Hoteles.query.all()  # Obtener todos los hotel
+    hoteles_serialize = [hotel.serialize() for hotel in hoteles]  # Serializar cada hotel
+    return jsonify(hoteles_serialize), 200  # Retornar los datos serializados como JSON
+
+@api.route('/hoteles', methods=['POST'])
+def crear_hoteles():
+        data = request.get_json()
+        #crear hoteles
+        existing_hotel = Hoteles.query.filter_by(nombre=data["nombre"]).first()
+        if existing_hotel:
+            return jsonify({"error": "Hotel con este nombre ya existe"}), 400
+        
+        nuevo_hotel =Hoteles(
+            nombre=data["nombre"],
+            email=data["email"],
+        )
+        db.session.add(nuevo_hotel)
+        db.session.commit()
+    
+        return jsonify(nuevo_hotel.serialize()), 200
+
+@api.route("/hoteles/<int:id>", methods=["DELETE"])
+def delete_hoteles(id):
+    hotel = Hoteles.query.get(id)
+    
+    if not hotel:
+        return jsonify({"error": "Hotel no encontrado"}), 400
+
+    db.session.delete(hotel)
+    db.session.commit()
+
+    return jsonify({"message" : "Hotel eliminado"}), 200
+
+@api.route("/hoteles/<int:id>", methods=["PUT"])
+def actualizar_hoteles(id):
+    hotel = Hoteles.query.get(id)
+    
+    if not hotel:
+        return jsonify({"error": "Hotel no encontrado"}), 400
+
+    data = request.get_json()
+
+    hotel.nombre = data.get("nombre", hotel.nombre)
+    hotel.email = data.get("email", hotel.email)
+
+    db.session.commit()
+
+    return jsonify(hotel.serialize()), 200
