f1b465eaccb3aa9a72b1a7865a3178dc64d702f0
Monique Menezes <122538719+Niquemenezes@users.noreply.github.com>
2025-03-25 13:01:50 +0000

    mantenito

--- src/front/js/component/chatBot.js
+++ src/front/js/component/chatBot.js
@@ -15,7 +15,7 @@ const Chatbot = () => {
     setError(null);
 
     try {
-      const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/chat`, {
+      const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/chat`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message }),
