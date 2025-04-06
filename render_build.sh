a46239373c7f190a7caf5b4d77a1e3ed2d824a5e
Monique Menezes <122538719+Niquemenezes@users.noreply.github.com>
2025-04-06 15:38:41 +0000

    Fix: usar pipenv con start y migraciones

--- render_build.sh
+++ render_build.sh
@@ -2,20 +2,9 @@
 # exit on error
 set -o errexit
 
-# Variables necesarias para Flask
-export FLASK_APP=src/app.py
-export FLASK_ENV=development
-export PYTHONPATH="$PYTHONPATH:./src"
-
-# Instalar frontend
 npm install
 npm run build
 
-# Instalar dependencias con pipenv
-pipenv install --deploy
+pipenv install
 
-# Ejecutar migraciones
-pipenv run flask db upgrade
-
-# Ejecutar la app
-pipenv run start
+pipenv run upgrade
